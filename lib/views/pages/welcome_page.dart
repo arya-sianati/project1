3641bafc10705a7025cca3b1608c5905dd3d0372
arya <arya@Aryas-MacBook-Pro.local>
2025-04-29 22:27:31 -0700

    Add dark/light mode to the welcome page

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/data/notifiers.dart';
 import 'package:project1/views/widget_tree.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -10,9 +11,19 @@ class WelcomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-          child: Column(
+          child: ValueListenableBuilder(valueListenable: isDarkModeNotifier, builder: (context, isDarkMode, child) {
+            
+            return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // Text(''),
+              Image.asset(
+                    'assets/images/chess1.png',
+                    width: 300,
+                    height: 300,
+                    color: isDarkMode ? Colors.white : Colors.black,
+                  ),
+              const SizedBox(height: 5),
               FilledButton(
                 onPressed: () {
                   Navigator.push(
@@ -20,10 +31,29 @@ class WelcomePage extends StatelessWidget {
                     MaterialPageRoute(builder: (context) => const WidgetTree()),
                   );
                 },
+                style: FilledButton.styleFrom(
+                  backgroundColor: isDarkMode ? Colors.white : Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  minimumSize: const Size(200, 50),
+                ),
                 child: const Text('Get Started'),
               ),
+
+              const SizedBox(height: 5),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const WidgetTree()),
+                  );
+                },
+                child: const Text('Login'),
+              ),
             ],
-          ),
+          );
+          },)
         ),
       ),
     );
